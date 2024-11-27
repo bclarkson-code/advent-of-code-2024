28939251974d2779819d5d97c94853661ca91423
Clarkson, Benedict <benedict.clarkson@sky.uk>
2024-11-27 20:36:52 +0000

    Owrking on previous day 1

--- setup/previous_day_1.exs
+++ setup/previous_day_1.exs
@@ -40,12 +40,48 @@ defmodule Solution do
     end
   end
 
-  defp parse_words(line, total) do
-    digits = "0123456789"
-    line = String.graphemes(line)
+  defp parse_words([], total), do: nil
 
-    first = first_occurrence(line, digits)
-    last = first_occurrence(Enum.reverse(line), digits)
+  defp parse_words(line, total) do
+    allowed = %{
+      "0" => 0,
+      "1" => 1,
+      "2" => 2,
+      "3" => 3,
+      "4" => 4,
+      "5" => 5,
+      "6" => 6,
+      "7" => 7,
+      "8" => 8,
+      "9" => 9,
+      "zero" => 0,
+      "one" => 1,
+      "two" => 2,
+      "three" => 3,
+      "four" => 4,
+      "five" => 5,
+      "six" => 6,
+      "seven" => 7,
+      "eight" => 8,
+      "nine" => 9
+    }
+
+    allowed_pattern = ~r/\d|zero|one|two|three|four|five|six|seven|eight|nine/
+    matches = Regex.scan(allowed_pattern, line)
+    IO.puts("#{matches}")
+
+    case matches do
+      [first | _] -> first
+      nil -> first = nil
+    end
+
+    case Enum.reverse(matches) do
+      [last | _] -> last
+      nil -> last = nil
+    end
+
+    first = Map.get(allowed, first)
+    last = Map.get(allowed, last)
 
     with f when not is_nil(f) <- first,
          l when not is_nil(l) <- last do
@@ -64,19 +100,20 @@ defmodule Solution do
     data = read_file("setup/day_1_input.txt")
 
     String.split(data, "\n")
-    |> Enum.reduce(0, &parse_line/2)
+    |> Enum.reduce(0, &parse_digits/2)
   end
 
   def part_2() do
     data = read_file("setup/day_1_input.txt")
 
-    String.split(data, "\n")
-    |> Enum.reduce(0, &parse_words/2)
+    allowed =
+      String.split(data, "\n")
+      |> Enum.reduce(0, &parse_words/2)
   end
 end
 
 out = Solution.part_1()
 IO.puts(out)
 
-out = Solution.part_1()
+out = Solution.part_2()
 IO.puts(out)
