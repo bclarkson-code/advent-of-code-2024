d9ef528ea86dc93b0ebba716b1ead7bfbd08550c
Clarkson, Benedict <benedict.clarkson@sky.uk>
2024-11-28 10:33:35 +0000

    Cleaned up previous day 1 solution

--- setup/previous_day_1.exs
+++ setup/previous_day_1.exs
@@ -1,91 +1,83 @@
 defmodule Solution do
-  defp read_file(file_name) do
-    case File.read(file_name) do
-      {:ok, data} ->
-        data
-
-      {:error, reason} ->
-        IO.puts(reason)
-        raise reason
-    end
-  end
-
-  defp parse_digits([], total), do: total
-
-  defp parse_digits(line, total) do
-    numbers =
-      Regex.scan(~r/\d/, line)
-      |> Enum.map(fn [val] ->
-        {parsed, _} = Integer.parse(val)
-        parsed
-      end)
-
-    case numbers do
-      [] ->
-        total
-
-      _ ->
-        first = List.first(numbers)
-        last = List.last(numbers)
-        first * 10 + last + total
-    end
-  end
-
-  defp parse_words([], total), do: total
-
-  defp parse_words(line, total) do
-    allowed = %{
-      "0" => 0,
-      "1" => 1,
-      "2" => 2,
-      "3" => 3,
-      "4" => 4,
-      "5" => 5,
-      "6" => 6,
-      "7" => 7,
-      "8" => 8,
-      "9" => 9,
-      "zero" => 0,
-      "one" => 1,
-      "two" => 2,
-      "three" => 3,
-      "four" => 4,
-      "five" => 5,
-      "six" => 6,
-      "seven" => 7,
-      "eight" => 8,
-      "nine" => 9
-    }
-
-    numbers =
-      Enum.flat_map(allowed, fn {word, value} ->
-        Regex.scan(~r/#{word}/, line, return: :index)
-        |> Enum.map(fn [{start, _len}] -> {start, value} end)
-      end)
-      |> Enum.sort()
-      |> Enum.map(fn {_idx, val} -> val end)
-
-    case numbers do
-      [] ->
-        total
-
-      _ ->
-        first = List.first(numbers)
-        last = List.last(numbers)
-        first * 10 + last + total
-    end
-  end
+  @allowed %{
+    "0" => 0,
+    "1" => 1,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "zero" => 0,
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9
+  }
 
   def part_1() do
     read_file("setup/day_1_input.txt")
-    |> String.split(data, "\n")
-    |> Enum.reduce(0, &parse_digits/2)
+    |> Enum.map(&parse_words/1)
+    |> Enum.sum()
   end
 
   def part_2() do
     read_file("setup/day_1_input.txt")
-    |> String.split("\n")
-    |> Enum.reduce(0, &parse_words/2)
+    |> Enum.map(&parse_digits/1)
+    |> Enum.sum()
+  end
+
+  defp read_file(file_name) do
+    file_name
+    |> File.read!()
+    |> String.split("\n", trim: true)
+  end
+
+  defp combine_first_and_last([]), do: 0
+
+  defp combine_first_and_last(extracted) do
+    first = List.first(extracted)
+    last = List.last(extracted)
+    first * 10 + last
+  end
+
+  defp extract_digits(line) do
+    ~r/\d/
+    |> Regex.scan(line)
+    |> Enum.map(fn [val] -> String.to_integer(val) end)
+  end
+
+  defp extract_words(line) do
+    @allowed
+    |> Enum.flat_map(fn {word, value} ->
+      Regex.scan(~r/#{word}/, line, return: :index)
+      |> Enum.map(fn [{idx, _len}] -> {idx, value} end)
+    end)
+    |> Enum.sort()
+    |> Enum.map(&elem(&1, 1))
+  end
+
+  defp parse_digits([]), do: 0
+
+  defp parse_digits(line) do
+    line
+    |> extract_digits()
+    |> combine_first_and_last()
+  end
+
+  defp parse_words([]), do: 0
+
+  defp parse_words(line) do
+    line
+    |> extract_words()
+    |> combine_first_and_last()
   end
 end
 
