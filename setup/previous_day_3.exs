ea335fea44529d4be8d6a12a6754553b1da73be3
Clarkson, Benedict <benedict.clarkson@sky.uk>
2024-11-29 11:23:32 +0000

    Added skeleton for day 3

---
+++ setup/previous_day_3.exs
@@ -0,0 +1,25 @@
+defmodule Solution do
+  @day -1
+
+  @suffix "_sample"
+
+  def part_1() do
+    read_file("setup/day_#{@day}#{@suffix}_input.txt")
+  end
+
+  def part_2() do
+    read_file("setup/day_#{@day}#{@suffix}_input.txt")
+  end
+
+  defp read_file(file_name) do
+    file_name
+    |> File.read!()
+    |> String.split("\n", trim: true)
+  end
+end
+
+out = Solution.part_1()
+IO.puts(out)
+
+out = Solution.part_2()
+IO.puts(out)
